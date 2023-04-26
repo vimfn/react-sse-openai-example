7918c45e687e8c0654ebc417021881cfc8887c3b
its-ag <itsag0024@gmail.com>
2023-04-26 13:18:14 +0530

    feat: init

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,135 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading, Text, Box, Flex, Button, Textarea } from "@chakra-ui/react";
+import { useEffect, useState, useRef } from "react";
+import { SSE } from "sse";
+import { useToast } from "@chakra-ui/react";
+
+const API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
+console.log(API_KEY);
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [prompt, setPrompt] = useState("");
+  let [response, setResponse] = useState("");
+  let [isLoading, setIsLoading] = useState(false);
+
+  const responseRef = useRef(response);
+  const toast = useToast();
+
+  useEffect(() => {
+    responseRef.current = response;
+  }, [response]);
+
+  let handleClearBtnClicked = () => {
+    setPrompt("");
+    setResponse("");
+  };
+
+  let handlePromptChange = (e) => {
+    setPrompt(e.target.value);
+  };
+
+  let handleSubmitBtnClicked = async () => {
+    if (prompt === "") {
+      toast({
+        // title: "Error",
+        description: "Please enter a prompt",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
+    } else {
+      setIsLoading(true);
+      let url = "https://api.openai.com/v1/completions";
+      let data = {
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 100,
+        temperature: 0.9,
+        top_p: 1,
+        n: 1,
+        stream: true,
+      };
+
+      let source = new SSE(url, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${API_KEY}`,
+        },
+        method: "POST",
+        payload: JSON.stringify(data),
+      });
+
+      source.addEventListener("message", (e) => {
+        console.log("Message: ", e.data);
+        if (e.data != "[DONE]") {
+          let payload = JSON.parse(e.data);
+          let text = payload.choices[0].text;
+          if (text != "\n") {
+            console.log("Text: ", text);
+            responseRef.current += text;
+            setResponse(responseRef.current);
+          } else {
+            source.close();
+          }
+        }
+      });
+
+      source.addEventListener("readystatechange", (e) => {
+        if (e.readyState >= 2) {
+          setIsLoading(false);
+        }
+      });
+
+      source.stream();
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Flex
+      width={"100vw"}
+      height={"100vh"}
+      alignContent={"center"}
+      justifyContent={"center"}
+      bgGradient={"linear(to-b, #964dd6, #997c7e)"}
+    >
+      <Box
+        boxShadow="dark-lg"
+        maxW="2xl"
+        m="auto"
+        bg={"white"}
+        p="20px"
+        borderRadius={"md"}
+      >
+        <Heading mb={4}>🤖 OpenAI Completions</Heading>
+        <Text mb={4}>
+          This is an example of using SSE (Server-Sent Events) with React, Vite.
+        </Text>
+        <Textarea
+          value={prompt}
+          onChange={handlePromptChange}
+          placeholder={"Give me any sentence to complete.."}
+        />
+        <Button
+          mt={4}
+          isLoading={isLoading}
+          loadingText="Fetching Data.."
+          onClick={handleSubmitBtnClicked}
+          colorScheme={"purple"}
+        >
+          Ask
+        </Button>
+        <Button mt={4} mx={4} onClick={handleClearBtnClicked}>
+          Clear
+        </Button>
+
+        {response === "" ? null : (
+          <Box mt={4}>
+            <Heading fontSize={"md"}>Response</Heading>
+            <Text mt={2}>{response}</Text>
+          </Box>
+        )}
+      </Box>
+    </Flex>
+  );
 }
 
-export default App
+export default App;
